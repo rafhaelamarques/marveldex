78109fd176fa045c763b73d9ea9426ef9df8bf3a
rafhaelamarques <rafaelamarques_z@hotmail.com>
2025-03-26 21:20:55 -0300

    feat: implementa HomeBloc com events e states para character fetching

---
+++ lib/ui/pages/home/bloc/home_bloc.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:marveldex/data/model/character.dart';
+import 'package:marveldex/data/services/client/characters_client_local.dart';
+
+part 'home_event.dart';
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  final CharactersClientLocal characterClient;
+
+  HomeBloc({required this.characterClient}) : super(const HomeState()) {
+    on<HomeFetchCharactersEvent>((event, emit) async {
+      await _fetchCharacters(emit);
+    });
+    on<HomeFetchMoreCharactersEvent>((event, emit) async {
+      await _fetchCharacters(emit, offset: state.offset + 20);
+    });
+  }
+
+  Future<void> _fetchCharacters(
+    Emitter<HomeState> emit, {
+    int offset = 0,
+  }) async {
+    emit(state.copyWith(status: HomeStatus.loading));
+
+    try {
+      final characters = await characterClient.getCharacters(offset: offset);
+      emit(state.copyWith(status: HomeStatus.success, characters: characters));
+    } catch (e) {
+      emit(
+        state.copyWith(status: HomeStatus.failure, errorMessage: e.toString()),
+      );
+    }
+  }
+}
